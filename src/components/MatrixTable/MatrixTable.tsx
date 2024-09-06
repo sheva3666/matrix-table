ab68b24a6f8d63fcdb16d43d5b5a75d8272c6186
sheva3666 <ivan.shevchenko@sap.com>
2024-09-06 23:18:13 +0200

    removed fragment

--- src/components/MatrixTable/MatrixTable.tsx
+++ src/components/MatrixTable/MatrixTable.tsx
@@ -9,21 +9,19 @@ const MatrixTable = () => {
   const { matrix } = useMatrix();
 
   return (
-    <>
-      <table className="table">
-        <thead>
-          <MatrixHeaders />
-        </thead>
-        <tbody>
-          {matrix.map((row: Cell[], rowIndex: number) => (
-            <MatrixRow key={rowIndex} row={row} rowIndex={rowIndex} />
-          ))}
-        </tbody>
-        <tfoot>
-          <PrecentilesRow />
-        </tfoot>
-      </table>
-    </>
+    <table className="table">
+      <thead>
+        <MatrixHeaders />
+      </thead>
+      <tbody>
+        {matrix.map((row: Cell[], rowIndex: number) => (
+          <MatrixRow key={rowIndex} row={row} rowIndex={rowIndex} />
+        ))}
+      </tbody>
+      <tfoot>
+        <PrecentilesRow />
+      </tfoot>
+    </table>
   );
 };
 
